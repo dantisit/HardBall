fced6b80a38280ea7ee90a2125ae44ea3ecd4c12
Napoleonsha <amnoli3847@gmail.com>
2025-09-13 23:36:25 +0200

    настройки графики + счетчик хп на экране

---
+++ Assets/Scenes/Dev_Marina/Scripts/Hearts_UI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartsUI_Auto : MonoBehaviour
+{
+    public PlayerHealth playerHealth;   
+    public GameObject heartPrefab;      
+    public Sprite fullHeart;            
+    public Sprite emptyHeart;           
+
+    private Image[] hearts;             
+
+    void Start()
+    {
+        hearts = new Image[(int)playerHealth.MaxHealth];
+
+        for (int i = 0; i < playerHealth.MaxHealth; i++)
+        {
+            GameObject heart = Instantiate(heartPrefab, transform);
+
+            Image heartImage = heart.GetComponent<Image>();
+            hearts[i] = heartImage;
+        }
+    }
+
+    void Update()
+    {
+        bool isDead = playerHealth.DeathMenu.activeSelf;
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < Mathf.FloorToInt(playerHealth.CurrentHealth) && !isDead)
+                hearts[i].sprite = fullHeart;
+            else
+                hearts[i].sprite = emptyHeart;
+        }
+    }
+}
